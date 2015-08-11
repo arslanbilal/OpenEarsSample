afbb53d2142f5f4bb17c54f7f4d847297459698e
Bilal Arslan <bilal.arslan1993@hotmail.com>
2015-08-11 12:28:40 +0300

    Project and framework added

---
+++ TTSSampleDemo/TTSSampleDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TTSSampleDemo
+//
+//  Created by Bilal Arslan on 11/08/15.
+//  Copyright (c) 2015 Bilal Arslan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TTSSampleDemo/TTSSampleDemo/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  TTSSampleDemo
+//
+//  Created by Bilal Arslan on 11/08/15.
+//  Copyright (c) 2015 Bilal Arslan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self.view setBackgroundColor:[UIColor whiteColor]];
+    
+    
+    
+}
+
+
+
+
+@end
